236bffccc12d4f3e99204fc91a88de9d3d189660
KaziJahin826 <61079725+KaziJahin826@users.noreply.github.com>
2020-02-17 15:59:52 +0600

    Add files via upload

---
+++ Lesson2a.py
@@ -0,0 +1,33 @@
+print("This is second class")
+a= "Ali"
+b= 5
+c= 2.66 
+print('a')
+print('b')
+print('c')
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+p,q,r = 56,34.2,"Hi"
+
+print(p,q,r)
+
+print("p=%s and q=%s and r=%s"%(p,q,r))
+
+print("p={0} and q={2} and r={2}".format(p,q,r))
+
+
+print("p={} and q={} and r={}".format(p,q,r))
+
+
+print("p="+str(P)+" and q="+str(q)+" and r="+r+")
+
+print("We love {}".format("Bikash"))
+
+age=13
+
+message="Happy Basins "+str(age)+ "th Birthdayprint"
+
